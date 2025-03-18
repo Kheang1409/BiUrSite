e7375eafe85ab1d87bf80d1d7c722f22684de09d
Kheang1409 <40851151+Kheang1409@users.noreply.github.com>
2025-03-18 12:14:59 -0500

    Create signal-r.service.ts

---
+++ frontend/src/app/services/signal-r.service.ts
@@ -0,0 +1,112 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import * as signalR from '@microsoft/signalr';
+import { BehaviorSubject } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AuthService } from './auth.service';
+import { Post } from '../classes/post';
+import { Notification } from '../classes/notification';
+import { PostsDataService } from './posts-data.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SignalRService implements OnDestroy {
+  private _baseUrl = environment.urlApi.baseUrl;
+  private _notificationHub = environment.urlApi.notificationHub;
+
+  private hubConnection: signalR.HubConnection;
+
+  public notifications$ = new BehaviorSubject<Notification[]>([]);
+  public posts$ = new BehaviorSubject<Post[]>([]);
+  public connectionState$ = new BehaviorSubject<signalR.HubConnectionState>(signalR.HubConnectionState.Disconnected);
+
+  constructor(
+    private _authService: AuthService,
+    private _postService: PostsDataService
+  ) {
+    this.hubConnection = new signalR.HubConnectionBuilder()
+      .withUrl(`${this._baseUrl}${this._notificationHub}`, {
+        accessTokenFactory: () => {
+          const userId = this._authService.getToken()
+          return userId || '';
+        },
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.onclose(() => {
+      this.connectionState$.next(signalR.HubConnectionState.Disconnected);
+    });
+
+    this.hubConnection.onreconnecting(() => {
+      this.connectionState$.next(signalR.HubConnectionState.Reconnecting);
+    });
+
+    this.hubConnection.onreconnected(() => {
+      this.connectionState$.next(signalR.HubConnectionState.Connected);
+    });
+  }
+
+  public startConnection(): void {
+    if (
+      this.hubConnection.state === signalR.HubConnectionState.Connected ||
+      this.hubConnection.state === signalR.HubConnectionState.Connecting
+    ) {
+      return;
+    }
+
+    this.hubConnection
+      .start()
+      .then(() => {
+        this.connectionState$.next(signalR.HubConnectionState.Connected);
+      })
+      .catch((err) => {
+        console.error('Error while starting SignalR connection: ', err);
+        this.connectionState$.next(signalR.HubConnectionState.Disconnected);
+      });
+  }
+
+  public addNotificationListener(): void {
+    this.hubConnection.on('ReceiveNotification', (notification: Notification) => {
+      if (notification) {
+        this.notifications$.next([notification]); 
+      } else {
+        console.error('Received null or undefined notification.');
+      }
+    });
+  }
+
+  public addPostListener(): void {
+    this.hubConnection.on('ReceivePost', (post: Post) => {
+      console.log('New post received:', post);
+      if (post) {
+        // Fetch additional post details if needed
+        this._postService.getPost(post.postId).subscribe({
+          next: (newPost) => {
+            this.posts$.next([newPost]);
+          },
+          error: (error) => {
+            console.error('Error fetching post details:', error);
+          },
+        });
+      } else {
+        console.error('Received null or undefined post.');
+      }
+    });
+  }
+
+  public stopConnection(): void {
+    this.hubConnection
+      .stop()
+      .then(() => {
+        this.connectionState$.next(signalR.HubConnectionState.Disconnected);
+      })
+      .catch((err) => {
+        console.error('Error while stopping SignalR connection: ', err);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.stopConnection();
+  }
+}
